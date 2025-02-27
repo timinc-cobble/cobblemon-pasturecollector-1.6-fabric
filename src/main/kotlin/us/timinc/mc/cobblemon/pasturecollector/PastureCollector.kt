2ed46e44a1d0dc86bfb65a607ec548c1ec827b92
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-02-26 23:26:47 -0800

    barebones

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/pasturecollector/PastureCollector.kt
@@ -0,0 +1,9 @@
+package us.timinc.mc.cobblemon.pasturecollector
+
+import net.fabricmc.api.ModInitializer
+
+
+object PastureCollector : ModInitializer {
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/us/timinc/mc/cobblemon/pasturecollector/api/Config.kt
@@ -0,0 +1,5 @@
+package us.timinc.mc.cobblemon.pasturecollector.api
+
+open class Config {
+    val debug: Boolean = false
+}
