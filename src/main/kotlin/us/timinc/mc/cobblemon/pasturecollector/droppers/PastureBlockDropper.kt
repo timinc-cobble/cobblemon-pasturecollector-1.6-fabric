5f705f045e6d92ab5ff09fdfeb283045783adb0c
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-02-28 16:35:27 -0800

    pushed modId down to drop loot tables

--- src/main/kotlin/us/timinc/mc/cobblemon/pasturecollector/droppers/PastureBlockDropper.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/pasturecollector/droppers/PastureBlockDropper.kt
@@ -1,16 +1,8 @@
 package us.timinc.mc.cobblemon.pasturecollector.droppers
 
-import com.cobblemon.mod.common.pokemon.FormData
-import us.timinc.mc.cobblemon.pasturecollector.PastureCollector.modIdentifier
 import us.timinc.mc.cobblemon.droploottables.api.droppers.AbstractFormDropper
+import us.timinc.mc.cobblemon.pasturecollector.PastureCollector.MOD_ID
 
-object PastureBlockDropper : AbstractFormDropper("collector") {
+object PastureBlockDropper : AbstractFormDropper("collector", MOD_ID) {
     override fun load() {}
-
-    // TODO: Push these back down to Drop Loot Tables, and add a modId param.
-    override fun getFormDropId(form: FormData) =
-        modIdentifier("$dropType/${form.species.resourceIdentifier.path}${if (form.name != "Normal") "/${form.name.lowercase()}" else ""}")
-
-    override fun getAllDropId() =
-        modIdentifier("$dropType/all")
 }
